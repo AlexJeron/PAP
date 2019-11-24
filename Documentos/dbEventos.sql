7839d6c93fcac17c525b1525074a5c0e916bccc2
Alex <alexandrejeronimo2001@gmail.com>
2019-11-24 19:59:10 +0000

    update

---
+++ Documentos/dbEventos.sql
@@ -0,0 +1,96 @@
+create database dbEventos;
+
+use dbEventos;
+
+create table utilizador (
+utilizador_id int auto_increment,
+nome varchar(50) not null,
+password varchar(250) not null,
+email varchar(100) not null,
+primary key (utilizador_id)
+);
+
+create table espectador (
+espectador_id int auto_increment,
+numero_alunos int,
+numero_professores int,
+numero_pais int,
+outros varchar(250),
+primary key (espectador_id)
+);
+
+create table local (
+local_id int auto_increment,
+nome varchar(50) not null,
+primary key (local_id)
+);
+
+create table recurso (
+recurso_id int auto_increment,
+nome varchar(50) not null,
+primary key (recurso_id)
+);
+
+create table professor (
+professor_id int auto_increment,
+nome varchar(50) not null,
+primary key (professor_id)
+);
+
+create table turma (
+turma_id int auto_increment,
+nome_turma varchar(10) not null,
+primary key (turma_id)
+);
+
+create table evento (
+evento_id int auto_increment,
+local_id int,
+utilizador_id int,
+espectador_id int,
+inicio datetime,
+fim datetime,
+atividade varchar(250),
+foreign key (local_id) references local (local_id),
+foreign key (utilizador_id) references utilizador (utilizador_id),
+foreign key (espectador_id) references espectador (espectador_id),
+primary key (evento_id)
+);
+
+create table recursos_necessarios (
+recurso_id int,
+evento_id int,
+quantidade int,
+foreign key (recurso_id) references recurso (recurso_id),
+foreign key (evento_id) references evento (evento_id),
+primary key (recurso_id, evento_id)
+);
+
+create table professor_evento (
+professor_id int,
+evento_id int,
+foreign key (professor_id) references professor (professor_id),
+foreign key (evento_id) references evento (evento_id),
+primary key (professor_id, evento_id)
+);
+
+create table turma_evento (
+turma_id int,
+evento_id int,
+foreign key (turma_id) references turma (turma_id),
+foreign key (evento_id) references evento (evento_id),
+primary key (turma_id, evento_id)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
