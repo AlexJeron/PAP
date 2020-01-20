ed070298aa4140ccd8201474849f9edc8d50603b
Alex <alexandrejeronimo2001@gmail.com>
2020-01-20 09:28:23 +0000

    Update footer.blade.php

--- BiblioRaul/resources/views/footer.blade.php
+++ BiblioRaul/resources/views/footer.blade.php
@@ -1,9 +1,9 @@
 @extends('layout')
 
 <footer class="sticky-footer bg-white">
-  <div class="container my-auto">
-    <div class="copyright text-center my-auto">
-      <span>Copyright &copy; Your Website 2019</span>
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; </span>
+        </div>
     </div>
-  </div>
 </footer>
