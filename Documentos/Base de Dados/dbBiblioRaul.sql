bcac112ed5be036ef607b27e8fa7b7495a837861
Alex <alexandrejeronimo2001@gmail.com>
2020-01-23 16:47:18 +0000

    db.sql update

--- Documentos/Base de Dados/dbBiblioRaul.sql
+++ Documentos/Base de Dados/dbBiblioRaul.sql
@@ -1,23 +1,19 @@
-CREATE DATABASE dbeventostemp;
+CREATE DATABASE dbatividadestemp;
 
-USE dbeventostemp;
+USE dbatividadestemp;
 
 CREATE TABLE user (
   user_id int AUTO_INCREMENT,
   nome varchar(50) NOT NULL,
   PASSWORD varchar(255) NOT NULL,
   email varchar(255) NOT NULL,
-  remember_token varchar(255),
   PRIMARY KEY (user_id)
 );
 
-CREATE TABLE total_espectadores (
-  total_espectadores_id int AUTO_INCREMENT,
-  numero_alunos int,
-  numero_professores int,
-  numero_pais int,
-  outros varchar(255),
-  PRIMARY KEY (total_espectadores_id)
+CREATE TABLE outros_espectadores (
+  outros_espectadores_id int AUTO_INCREMENT,
+  descricao varchar(255) NOT NULL,
+  PRIMARY KEY (outros_espectadores_id)
 );
 
 CREATE TABLE LOCAL (
@@ -46,41 +42,51 @@ CREATE TABLE turma (
   PRIMARY KEY (turma_id)
 );
 
-CREATE TABLE evento (
-  evento_id int AUTO_INCREMENT,
+CREATE TABLE atividade (
+  atividade_id int AUTO_INCREMENT,
   user_id int NOT NULL,
-  total_espectadores_id int NOT NULL,
-  local_id int,
+  local_id int NOT NULL,
   inicio datetime NOT NULL,
   fim datetime,
-  atividade varchar(255) NOT NULL,
+  descricao varchar(255) NOT NULL,
+  notas varchar(255),
+  numero_alunos int,
+  numero_professores int,
+  numero_pais int,
   FOREIGN KEY (local_id) REFERENCES LOCAL (local_id),
   FOREIGN KEY (user_id) REFERENCES USER (user_id),
-  FOREIGN KEY (total_espectadores_id) REFERENCES total_espectadores (total_espectadores_id),
-  PRIMARY KEY (evento_id)
+  PRIMARY KEY (atividade_id)
 );
 
 CREATE TABLE recursos_necessarios (
   recurso_id int,
-  evento_id int,
+  atividade_id int,
   quantidade int,
   FOREIGN KEY (recurso_id) REFERENCES recurso (recurso_id),
-  FOREIGN KEY (evento_id) REFERENCES evento (evento_id),
-  PRIMARY KEY (recurso_id, evento_id)
+  FOREIGN KEY (atividade_id) REFERENCES atividade (atividade_id),
+  PRIMARY KEY (recurso_id, atividade_id)
 );
 
-CREATE TABLE professor_evento (
+CREATE TABLE professor_atividade (
   professor_id int,
-  evento_id int,
+  atividade_id int,
   FOREIGN KEY (professor_id) REFERENCES professor (professor_id),
-  FOREIGN KEY (evento_id) REFERENCES evento (evento_id),
-  PRIMARY KEY (professor_id, evento_id)
+  FOREIGN KEY (atividade_id) REFERENCES atividade (atividade_id),
+  PRIMARY KEY (professor_id, atividade_id)
 );
 
-CREATE TABLE turma_evento (
+CREATE TABLE turma_atividade (
   turma_id int,
-  evento_id int,
+  atividade_id int,
   FOREIGN KEY (turma_id) REFERENCES turma (turma_id),
-  FOREIGN KEY (evento_id) REFERENCES evento (evento_id),
-  PRIMARY KEY (turma_id, evento_id)
+  FOREIGN KEY (atividade_id) REFERENCES atividade (atividade_id),
+  PRIMARY KEY (turma_id, atividade_id)
+);
+
+CREATE TABLE outros_espectadores_atividade (
+  outros_espectadores_id int,
+  atividade_id int,
+  FOREIGN KEY (outros_espectadores_id) REFERENCES outros_espectadores (outros_espectadores_id),
+  FOREIGN KEY (atividade_id) REFERENCES atividade (atividade_id),
+  PRIMARY KEY (outros_espectadores_id, atividade_id)
 );
