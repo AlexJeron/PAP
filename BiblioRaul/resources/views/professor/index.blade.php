c98b8232ee0222fffa03611cadaa6f38d65d2f2a
Alex <alexandrejeronimo2001@gmail.com>
2020-01-28 16:59:39 +0000

    adicionar professor modal

--- BiblioRaul/resources/views/professor/index.blade.php
+++ BiblioRaul/resources/views/professor/index.blade.php
@@ -36,7 +36,22 @@
                 <!-- DataTales Example -->
                 <div class="card shadow mb-4">
                     <div class="card-header py-3">
-                        <h5 class="m-0 font-weight-bold text-primary">Gerir Professores</h5>
+                        <div class="row">
+                            <h5 class="col-9 d-flex p-2 m-0 font-weight-bold text-primary">
+                                Gerir Professores
+                                </h4>
+                                <div class="col-3">
+                                    <button type="button"
+                                        class="float-right d-flex d-sm-inline-block btn btn-sm btn-primary shadow-sm"
+                                        style="margin-top:0.3rem" data-toggle="modal" data-target="#newProfessorModal">
+                                        <i class="fas fa-plus-circle fa-sm text-white-50"></i> Adicionar
+                                        Professor</button>
+                                </div>
+                        </div>
+                        {{-- <h5 class="m-0 font-weight-bold text-primary"> --}}
+
+                        {{-- </h5> --}}
+
                     </div>
                     <div class="card-body">
                         <div class="table table-clear">
@@ -78,6 +93,55 @@
 
 </div>
 <!-- End of Page Wrapper -->
+
+<div class="modal fade" id="newProfessorModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Adicionar Professor</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form method="POST" action="/professor">
+                    @csrf
+                    <div class="form-group row">
+                        <label for="name" class="col-sm-2 col-form-label">{{ __('Nome') }}</label>
+                        <div class="col-sm-10">
+                            <input id="name" type="text" class="form-control @error('nome') is-invalid @enderror"
+                                name="nome" value="{{ old('name') }}" autocomplete="name" autofocus>
+
+                            @error('nome')
+                            <div class="invalid-feedback">{{ $errors->first('nome') }}</div>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="email" class="col-sm-2 col-form-label">{{ __('Email') }}</label>
+
+                        <div class="col-sm-10">
+                            <input id="email" type="text" class="form-control @error('email') is-invalid @enderror"
+                                name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
+
+                            @error('email')
+                            <div class="invalid-feedback">{{ $errors->first('email') }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">
+                    {{ __('Adicionar') }}
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
 
 @include('layouts.logout-modal')
 
